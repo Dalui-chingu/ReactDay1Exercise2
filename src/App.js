14b3b7447aa2645fe9161fc0edc9b9fcdcd8d823
Dalui-chingu <gowrisubha44@gmail.com>
2023-03-30 23:56:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1>Reference Data Types use  same memory</h1>
+        <button onClick={window['func']}>Click here to call function</button>
+      </header>
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import World from './components/World';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <World></World>
+    <App />
   </React.StrictMode>
 );
 
